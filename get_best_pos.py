d82cfe28eab9c70dab7390b5d678337ba04b2f6f
Jeff Doozan <github@doozan.com>
2020-04-21 10:04:35 -0500

    fixes

--- get_best_pos.py
+++ get_best_pos.py
@@ -1,6 +1,12 @@
 import spanish_words
 import spanish_sentences
 import spanish_lemmas
+import sys
+
+def dprint(debug, *args, **kwargs):
+    if debug:
+        print(*args, file=sys.stderr, **kwargs)
+
 
 def get_pos_overlap(word, debug=False):
     lemmadb_pos = spanish_lemmas.get_all_pos(word)
@@ -8,24 +14,18 @@ def get_pos_overlap(word, debug=False):
     sentences_pos = set(map(str.upper, spanish_sentences.get_all_pos(word)))
     dictionary_pos = set(spanish_words.get_all_pos(word))
 
-    if debug:
-        print(word, "lemmadb: ", lemmadb_pos)
-        print(word, "sentences: ", sentences_pos)
-        print(word, "words: ", dictionary_pos)
+    dprint(debug, word, "lemmadb: ", lemmadb_pos)
+    dprint(debug, word, "sentences: ", sentences_pos)
+    dprint(debug, word, "words: ", dictionary_pos)
 
-    # The dictionary usually has the best/most accurate data
-    # The lemmadb is okay but not great, is a good source of "most common" usage
-    # and the sentences data is often wrong
-    # If there's an agreement between any of the datasets, assume it's likely a good usage
+    # the sentences pulls from the same dataset (treetagger) as the lemmas, so agreement between the lemmadb and sentencesdb isn't worth much
+    # however, the sentences database has better context than the lemmadb and will sometimes have more results
+    # so it's good to check the dictionary against the lemmadb and the sentencesdb when looking for matches
 
     all_pos = sorted(lemmadb_pos & sentences_pos & dictionary_pos)
-    if not len(all_pos):
-        if len(sentences_pos & dictionary_pos):
-            print(word, sorted(sentences_pos & dictionary_pos), "has sentences and dictionary usage but no lemmadb")
-#        all_pos = sentences_pos & dictionary_pos
 
     if not len(all_pos):
-        all_pos = (lemmadb_pos & dictionary_pos) | (lemmadb_pos & sentences_pos)
+        all_pos = (dictionary_pos & lemmadb_pos) | (dictionary_pos & sentences_pos)
 
     return all_pos
 
@@ -39,7 +39,7 @@ def get_lemma_pos(word):
     search_lemmas = [""]
     lemmas = spanish_lemmas.get_all_pos(word)
     if lemmas and len(lemmas):
-        search_lemmas += sorted(lemmas)
+        search_lemmas += sorted(set(lemmas))
 
     for more_lemma in ["VERB", "NOUN", "ADJ"]:
         if more_lemma not in search_lemmas:
@@ -47,53 +47,61 @@ def get_lemma_pos(word):
 
     for lemma in search_lemmas:
         if lemma not in ["", "VERB", "NOUN", "ADJ"]:
-            search_lemmas.delete(lemma)
+            search_lemmas.remove(lemma)
+
+    return search_lemmas
 
 
 def get_best_pos(word, debug=False):
     # order here does not matter because every item will be assessed and scored
     # and the highest score will be selected
-    search_lemmas = ["", "ADJ", "NOUN", "VERB"]
+    #search_lemmas = ["", "ADJ", "NOUN", "VERB"]
+
+    search_lemmas = get_lemma_pos(word)
+    dprint(debug, "Searching: ", search_lemmas)
+
+    check_pos = []
+    for pos in spanish_lemmas.get_all_pos(word):
+        if pos not in check_pos:
+            check_pos.append(pos)
 
     for lemma_pos in search_lemmas:
         lemma = spanish_words.get_lemma(word, lemma_pos)
+
         all_pos = get_pos_overlap(lemma, debug)
-        if debug:
-            print("all_pos (%s %s %s): %s"%(word,lemma_pos, lemma, all_pos))
 
-        if all_pos and len(all_pos):
-            print("getting weights for: ", lemma, all_pos)
-        continue
+        if not all_pos or not len(all_pos):
+            dprint(debug, "no overlapping usage for %s as a %s"%(lemma, lemma_pos))
+            continue
 
+        #dprint(debug, "getting weights for (%s %s %s): %s"%(word,lemma_pos, lemma, all_pos))
+        dprint(debug, "getting weights for (%s %s %s): %s"%(word,lemma_pos, lemma, check_pos))
 
         # If there are multiple options, prefer the one with most sentence usage
         # skews us towards the tagging mistakes in the sentences (eg "placer" is
         # usually mis-tagged as a verb instead of a noun and will return "verb")
         pos  = ""
-        if len(all_pos) == 1:
-            pos = list(all_pos)[0]
-        elif len(all_pos) > 1:
-            if debug:
-                print("tie breaker")
-            #pos = spanish_sentences.get_best_pos(lemma, all_pos)
-            if debug:
-                print("result = ",pos)
+#        if len(all_pos) == 1:
+#            pos = list(all_pos)[0]
+#        elif len(all_pos) > 1:
+#            dprint(debug, "tie breaker")
+#            pos = spanish_sentences.get_best_pos(lemma, all_pos)
+        pos = spanish_sentences.get_best_pos(lemma, check_pos)
+        dprint(debug, "result = ",pos)
 
         if pos:
             return pos
 
-    exit()
-
     # There is no overlap between any sources for any available lemmmas
     # Search again using just the dictionary
     for lemma_pos in ["", "NOUN", "VERB", "ADJ"]:
-        lemma = spanish_words.get_lemma(word, pos)
-        dictionary_pos = sorted(set(spanish_words.get_all_pos(word)))
+        lemma = spanish_words.get_lemma(word, lemma_pos)
+        dictionary_pos = spanish_words.get_all_pos(word)
 
         if len(dictionary_pos):
             return dictionary_pos[0]
 
-    if pos == "":
-            pos = "NONE"
-#            print("NONE:", word)
-    return pos
+#    if pos == "":
+#            pos = "NONE"
+#            dprint(debug, "NONE:", word)
+    return "NONE"
