ef1654c24abd023188713d1a254710e24ff23a16
Jeff Doozan <github@doozan.com>
2020-07-20 11:14:52 -0500

    Update build instructions

--- build_spanish_data.sh
+++ build_spanish_data.sh
@@ -18,19 +18,50 @@ curl 'https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-
   | gawk -v LANG=Spanish -v ISO=es -v REMOVE_WIKILINKS="y" -v ENABLE_SYN="y" -v ENABLE_META="y" -v DEBUG_META="spanish_data/meta-templates.txt" -f trans-en-es.awk \
   > orig.es-en.txt
   sort -s -d -k 1,1 -t"{" -o orig.es-en.txt orig.es-en.txt
-  python3 cleanup_wiki.py orig.es-en.txt spanish_data/es-en.txt
+  python3 process_meta.py orig.es-en.txt > spanish_data/es-en.txt
 
-# process the pythonol dictionary into a list of synonyms
-curl https://github.com/phrozensmoke/LEGACY-Open_Source-Pre2006/raw/master/pythonol/dictionary/data_dict1.txt \
-  | iconv -f ISO-8859-1 -t UTF-8 \
-  | python3 build_synonyms.py \
-  > spanish_data/synonyms.txt
 
-# Download and tag the sentences
-curl wget http://www.manythings.org/anki/spa-eng.zip \
-  | zcat \
-  | python3 build_sentences.py \
-  > spanish_data/sentences.json
+# Build the sentence list
+wget https://downloads.tatoeba.org/exports/per_language/spa/spa_sentences_detailed.tsv.bz2
+wget https://downloads.tatoeba.org/exports/per_language/eng/eng_sentences_detailed.tsv.bz2
+wget https://downloads.tatoeba.org/exports/links.tar.bz2
+wget https://downloads.tatoeba.org/exports/user_languages.tar.bz2
+wget http://www.manythings.org/anki/spa-eng.zip
+
+unzip spa-eng.zip
+
+bzcat user_languages.tar.bz2 | tail -n +2 |  grep -P "^spa\t5" | cut -f 3 | grep -v '\N' > spa_5.txt
+bzcat user_languages.tar.bz2 | tail -n +2 |  grep -P "^spa\t4" | cut -f 3 | grep -v '\N' > spa_4.txt
+bzcat user_languages.tar.bz2 | tail -n +2 |  grep -P "^spa\t[1-5]" | cut -f 3 > spa_known.txt
+
+bzcat user_languages.tar.bz2 | tail -n +2 |  grep -P "^eng\t5" | cut -f 3 | grep -v '\N' > eng_5.txt
+bzcat user_languages.tar.bz2 | tail -n +2 |  grep -P "^eng\t4" | cut -f 3 | grep -v '\N' > eng_4.txt
+bzcat user_languages.tar.bz2 | tail -n +2 |  grep -P "^eng\t[1-5]" | cut -f 3  > eng_known.txt
+
+bzcat spa_sentences_detailed.tsv.bz2 | awk 'BEGIN {FS="\t"} NR==FNR{A[$1];next}($4 in A){print $1 "\t" $3 "\t" $4 "\t50" }' spa_5.txt - > spa_sentences.tsv
+bzcat spa_sentences_detailed.tsv.bz2 | awk 'BEGIN {FS="\t"} NR==FNR{A[$1];next}($4 in A){print $1 "\t" $3 "\t" $4 "\t40" }' spa_4.txt - >> spa_sentences.tsv
+bzcat spa_sentences_detailed.tsv.bz2 | awk 'BEGIN {FS="\t"} NR==FNR{A[$1];next}!($4 in A){print $1 "\t" $3 "\t" $4 "\t0" }' spa_known.txt - >> spa_sentences.tsv
+
+bzcat eng_sentences_detailed.tsv.bz2 | awk 'BEGIN {FS="\t"} NR==FNR{A[$1];next}($4 in A){print $1 "\t" $3 "\t" $4 "\t5" }' eng_5.txt - > eng_sentences.tsv
+bzcat eng_sentences_detailed.tsv.bz2 | awk 'BEGIN {FS="\t"} NR==FNR{A[$1];next}($4 in A){print $1 "\t" $3 "\t" $4 "\t4" }' eng_4.txt - >> eng_sentences.tsv
+bzcat eng_sentences_detailed.tsv.bz2 | awk 'BEGIN {FS="\t"} NR==FNR{A[$1];next}!($4 in A){print $1 "\t" $3 "\t" $4 "\t0" }' eng_known.txt - >> eng_sentences.tsv
+
+
+cat<<'EOF'>join.awk
+BEGIN {FS="\t"}
+FNR == 1 {FID++} 
+FID==1{eng[$1] = $2; credit[$1] = $3; skill[$1] = $4}
+FID==2{spa[$1] = $2; credit[$1] = $3; skill[$1] = $4}
+FID==3 && $1 in eng && $2 in spa{ print eng[$1] "\t" spa[$2] "\tCC-BY 2.0 (France) Attribution: tatoeba.org #" $1 " (" credit[$1] ") & #" $2 " (" credit[$2] ")\t" skill[$1]+skill[$2] }
+EOF
+
+bzcat links.tar.bz2 | gawk -f join.awk eng_sentences.tsv spa_sentences.tsv - | pv > joined.tsv
+
+cat spa.txt joined.tsv | sort -k1,1 -k2,2 -t$'\t' --unique | awk 'BEGIN {FS="\t"}; {x=$1; print gsub(/ /, " ", x) "\t" $0}' | sort -n | cut -f 2- | sed 's/)$/)\t56/' > eng-spa.tsv
+
+./build_sentences.py eng-spa.tsv > spa-only.txt
+./build_tags.sh spa-only.txt
+./build_sentences.py --tags tags.bz2 eng-spa.tsv > spanish_data/sentences.json
 
 # Generate the irregular verb list and table
 python3 build_irregular_verbs.py > spanish_words/paradigms.py
